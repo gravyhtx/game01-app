ff6a4a95602f863537504226c3c6507d011f2713
GravyHTX <godisgravy@gmail.com>
2020-02-18 12:10:25 -0600

    This is an example so far. It will be used as a foundation for building this game.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # game01-app
+This is the AR example code with floating box that rotates. Open HTML file and use marker in "MARKER" folder on your phone to see the magic.
+
+A private repository has been created to protect information in this game.
+
+Story and functionality of the game has been created and will be added later.
+
+*** This is only an example of functionality for AR.js ***
+
+
+
+
+More examples here
+https://stemkoski.github.io/AR-Examples/
+
+See "Smoothed Controls" for example of opacity which will be used in first step of game.
+
+
+**NOTES**
+    // Add new branches for new collaborators //
---
+++ events.js
@@ -0,0 +1,25 @@
+// THIS SCRIPT CONTAINS ALL JS EVENTS FOR THE PROJECT //
+// ------------------------------------------------- //
+
+
+// Event on-click from... //
+// https://rawgit.com/nicolocarpignoli/nicolocarpignoli.github.io/master/ar-click-events/events.js
+
+AFRAME.registerComponent('markerhandler', {
+
+    init: function() {
+        const animatedMarker = document.querySelector("#animated-marker");
+        const aEntity = document.querySelector("#animated-model");
+
+        // every click, we make our model grow in size :)
+        animatedMarker.addEventListener('click', function(ev, target){
+            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            if (aEntity && intersectedElement === aEntity) {
+                const scale = aEntity.getAttribute('scale');
+                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+                aEntity.setAttribute('scale', scale);
+            }
+        });
+}});
+
+
